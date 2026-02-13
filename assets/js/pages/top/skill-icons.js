6fff2a95bf5b5d88d3d974ec6c720f598c98bf50
めいびぃ <162483806+Maybe515@users.noreply.github.com>
2026-02-13 18:53:37 +0900

    Add files via upload

---
+++ assets/js/pages/top/skill-icons.js
@@ -0,0 +1,49 @@
+export const skillIcons = {
+    // frontend
+    HTML: "HTML.svg",
+    CSS: "CSS.svg",
+    JavaScript: "JavaScript.svg",
+    TypeScript: "TypeScript.svg",
+    jQuery: "JQuery.svg",
+    SVG: "SVG.svg",
+    Android: "Android.svg",
+    AppInventor: "MITAppInventor.svg",
+
+    // backend
+    C: "C.svg",
+    CPP: "CPP.svg",
+    CSharp: "CS.svg",
+    Java: "Java.svg",
+    Python: "Python.svg",
+    Ruby: "Ruby.svg",
+    nodejs: "NodeJS.svg",
+    npm: "Npm.svg",
+    SQLite: "SQLite.svg",
+    dotNET: "DotNet.svg",
+    DiscordBot: "DiscordBots.svg",
+    Discordjs: "DiscordJS.svg",
+    VBA: "VBA.svg",
+    RaspberryPi: "RaspberryPi.svg",
+
+    // tools
+    Windows: "Windows.svg",
+    Cmd: "Cmd.svg",
+    PowerShell: "Powershell.svg",
+    VSCode: "VSCode.svg",
+    VisualStudio: "VisualStudio.svg",
+    GitHub: "Github.svg",
+    Discord: "Discord.svg",
+    Blender: "Blender.svg",
+    Photoshop: "Photoshop.svg",
+    Illustrator: "Illustrator.svg",
+    Gimp: "Gimp.svg",
+    Inkscape: "Inkscape.svg",
+    XPPen: "XP-Pen.svg",
+    Wacom: "Wacom.svg",
+    CSP: "ClipStudioPaint.svg",
+    TourBox: "TourBox.svg"
+};
+
+
+
+
